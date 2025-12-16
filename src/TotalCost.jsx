386954abe198c815008b34344d318a4a03240b20
prakash <pmanicka2@gmail.com>
2025-12-16 15:18:30 -0500

    Second block of code

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -11,10 +11,12 @@ const TotalCost = ({ totalCosts, ItemsDisplay }) => {
           <p className="preheading"><h3>Total cost for the event</h3></p>
         </div>
         <div>
-          <h2 id="pre_fee_cost_display" className="price">
-           
+          <h2 id="pre_fee_cost_display" className="price">        
+          ${total_amount}
           </h2>
-         
+         <div className="render_items">
+            <ItemsDisplay />
+         </div>
             <div>
              
             </div>
